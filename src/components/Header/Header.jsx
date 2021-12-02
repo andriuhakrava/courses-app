4db11808bca8773aab20093549f83c12a5eec845
andriuhakrava <andriuhakrava@gmail.com>
2021-12-02 17:50:22 +0200

    add authentication logic to the header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useNavigate } from 'react-router';
 
 import { HeaderStyled, Content } from './Header.style.js';
 
 import Logo from './components/Logo/Logo.jsx';
 import Button from '../../common/Button/Button.jsx';
 
-const Header = () => (
-	<HeaderStyled>
-		<div className='container'>
-			<Content>
-				<Logo></Logo>
-				<div className='header__content'>
-					<span>Andrii</span>
-					<form>
-						<Button type='submit' buttonText='Logout' />
-					</form>
-				</div>
-			</Content>
-		</div>
-	</HeaderStyled>
-);
+const Header = () => {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const navigate = useNavigate();
+
+	const getToken = () => {
+		if (localStorage.getItem('userToken')) {
+			setIsAuthenticated(true);
+		}
+	};
+
+	const logOut = (e) => {
+		e.preventDefault();
+		localStorage.removeItem('userToken');
+		setIsAuthenticated(false);
+		navigate('/login');
+	};
+
+	useEffect(() => {
+		getToken();
+	});
+
+	return (
+		<HeaderStyled>
+			<div className='container'>
+				<Content>
+					<Logo></Logo>
+					<div className='header__content'>
+						<span>
+							{isAuthenticated && (
+								<div className='header__content'>
+									<span>Andrii</span>
+									<form>
+										<Button
+											type='button'
+											buttonText='Logout'
+											onClick={logOut}
+										/>
+									</form>
+								</div>
+							)}
+						</span>
+					</div>
+				</Content>
+			</div>
+		</HeaderStyled>
+	);
+};
 
 export default Header;
