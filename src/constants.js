88a55770c1315130e44967072edf6b8b8ebc1218
andriuhakrava <andriuhakrava@gmail.com>
2021-12-02 17:16:10 +0200

    fix course authors mocked ids

--- src/constants.js
+++ src/constants.js
@@ -31,10 +31,6 @@ specimen book.`,
 		authors: [
 			'df32994e-b23d-497c-9e4d-84e4dc02882f',
 			'095a1817-d45b-4ed7-9cf7-b2417bcbf748',
-			'df32994e-b23d-497c-9e4d-84e4dc02882f',
-			'095a1817-d45b-4ed7-9cf7-b2417bcbf748',
-			'df32994e-b23d-497c-9e4d-84e4dc02882f',
-			'095a1817-d45b-4ed7-9cf7-b2417bcbf748',
 		],
 	},
 ];
