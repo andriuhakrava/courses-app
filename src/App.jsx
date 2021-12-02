15b96a7d7ef47ea705822167ae79aec785e27271
andriuhakrava <andriuhakrava@gmail.com>
2021-12-02 17:41:45 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css';
 
-import Header from './components/Header/Header.jsx';
+import { Routes, Route } from 'react-router-dom';
+
+import Layout from './components/Layout/Layout.jsx';
 import Courses from './components/Courses/Courses.jsx';
+import Registration from './components/Registration/Registration.jsx';
+import Login from './components/Login/Login.jsx';
+import CreateCourse from './components/CreateCourse/CreateCourse.jsx';
+import CourseInfo from './components/Courses/components/CourseInfo/CourseInfo.jsx';
 
 const App = () => (
 	<>
-		<Header />
-		<Courses />
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<Courses />} />
+				<Route path='registration' element={<Registration />} />
+				<Route path='login' element={<Login />} />
+				<Route path='courses' element={<Courses />} />
+				<Route
+					path='courses/add'
+					element={<CreateCourse courseDurationDefault='00:00' />}
+				/>
+				<Route path='courses/:courseId' element={<CourseInfo />} />
+			</Route>
+		</Routes>
 	</>
 );
 
