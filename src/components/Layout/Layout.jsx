24816cb8a195ee91f2133761cd796cb843b59aba
andriuhakrava <andriuhakrava@gmail.com>
2021-12-06 15:10:38 +0200

    remove header component from project layout page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 
-import Header from '../../components/Header/Header.jsx';
-
-const Layout = () => (
-	<>
-		<Header />
-		<Outlet />
-	</>
-);
+const Layout = () => <Outlet />;
 
 export default Layout;
