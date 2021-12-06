33e485be27be475ecefa540d6379c22ae2ebf04d
andriuhakrava <andriuhakrava@gmail.com>
2021-12-06 15:19:05 +0200

    ädd token checking to the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 import { Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
+import { getToken } from './helpers/localStorageHandler';
+
+import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout.jsx';
 import Courses from './components/Courses/Courses.jsx';
 import Registration from './components/Registration/Registration.jsx';
@@ -9,22 +13,43 @@ import Login from './components/Login/Login.jsx';
 import CreateCourse from './components/CreateCourse/CreateCourse.jsx';
 import CourseInfo from './components/Courses/components/CourseInfo/CourseInfo.jsx';
 
-const App = () => (
-	<>
-		<Routes>
-			<Route path='/' element={<Layout />}>
-				<Route index element={<Courses />} />
-				<Route path='registration' element={<Registration />} />
-				<Route path='login' element={<Login />} />
-				<Route path='courses' element={<Courses />} />
-				<Route
-					path='courses/add'
-					element={<CreateCourse courseDurationDefault='00:00' />}
-				/>
-				<Route path='courses/:courseId' element={<CourseInfo />} />
-			</Route>
-		</Routes>
-	</>
-);
+const App = () => {
+	const token = getToken();
+	const location = useLocation();
+
+	if (!token) {
+		if (location.pathname === '/login') {
+			return <Login />;
+		}
+
+		if (location.pathname === '/registration') {
+			return <Registration />;
+		}
+	}
+
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={token ? <Courses /> : <Login />} />
+					<Route path='login' element={<Login />} />
+					<Route path='registration' element={<Registration />} />
+					<Route path='courses' element={token ? <Courses /> : <Login />} />
+					<Route
+						path='courses/add'
+						element={
+							token ? <CreateCourse courseDurationDefault='00:00' /> : <Login />
+						}
+					/>
+					<Route
+						path='courses/:courseId'
+						element={token ? <CourseInfo /> : <Login />}
+					/>
+				</Route>
+			</Routes>
+		</>
+	);
+};
 
 export default App;
