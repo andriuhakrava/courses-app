0c56f52e0580a45aacf2cf982d46b35c5ca0ac25
andriuhakrava <andriuhakrava@gmail.com>
2021-12-09 12:17:35 +0200

    add token checking, reorganize routes, add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
 import './App.css';
+import React, { useEffect } from 'react';
 
-import { Routes, Route } from 'react-router-dom';
-import { useLocation } from 'react-router';
+import { Navigate, Routes, Route } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 import { getToken } from './helpers/localStorageHandler';
 
 import Header from './components/Header/Header';
-import Layout from './components/Layout/Layout.jsx';
 import Courses from './components/Courses/Courses.jsx';
 import Registration from './components/Registration/Registration.jsx';
 import Login from './components/Login/Login.jsx';
 import CreateCourse from './components/CreateCourse/CreateCourse.jsx';
 import CourseInfo from './components/Courses/components/CourseInfo/CourseInfo.jsx';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage.jsx';
 
 const App = () => {
 	const token = getToken();
-	const location = useLocation();
+	const navigate = useNavigate();
 
-	if (!token) {
-		if (location.pathname === '/login') {
-			return <Login />;
-		}
+	useEffect(() => {
+		const token = getToken();
 
-		if (location.pathname === '/registration') {
-			return <Registration />;
+		if (!token) {
+			navigate('/login');
 		}
-	}
+	}, [navigate, token]);
 
 	return (
 		<>
 			<Header />
 			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index element={token ? <Courses /> : <Login />} />
-					<Route path='login' element={<Login />} />
-					<Route path='registration' element={<Registration />} />
-					<Route path='courses' element={token ? <Courses /> : <Login />} />
-					<Route
-						path='courses/add'
-						element={
-							token ? <CreateCourse courseDurationDefault='00:00' /> : <Login />
-						}
-					/>
-					<Route
-						path='courses/:courseId'
-						element={token ? <CourseInfo /> : <Login />}
-					/>
-				</Route>
+				<Route
+					path='/'
+					element={token ? <Navigate to='/courses' /> : <Login />}
+				/>
+				<Route path='login' element={<Login />} />
+				<Route path='registration' element={<Registration />} />
+				<Route path='courses' element={token ? <Courses /> : <Login />} />
+				<Route
+					path='courses/add'
+					element={token ? <CreateCourse /> : <Login />}
+				/>
+				<Route
+					path='courses/:courseId'
+					element={token ? <CourseInfo /> : <Login />}
+				/>
+				<Route path='*' element={<NotFoundPage />} />
 			</Routes>
 		</>
 	);
