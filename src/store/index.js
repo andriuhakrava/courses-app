52d41cbbeefd21470ef64cb8110ad793c5bc8574
andriuhakrava <andriuhakrava@gmail.com>
2021-12-10 09:06:21 +0200

    rewrite initial store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -12,15 +12,8 @@ const rootReducer = combineReducers({
 	authors: authorsReducer,
 });
 
-function configureStore() {
-	const middlewareEnhancer = applyMiddleware(thunkMiddleware);
-	const composedEnhancers = composeWithDevTools(middlewareEnhancer);
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
-	const store = createStore(rootReducer, composedEnhancers);
-
-	return store;
-}
-
-const store = configureStore();
+const store = createStore(rootReducer, composedEnhancer);
 
 export default store;
