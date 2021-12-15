113e837616dd1b5bd1b0c21edaa3737bfeae320c
andriuhakrava <andriuhakrava@gmail.com>
2021-12-15 11:20:13 +0200

    add new selector for getting user role

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,2 +1,3 @@
 export const getAuthenticatedToken = (state) => state.user.token;
 export const getAuthenticatedUserName = (state) => state.user.name;
+export const getUserRole = (state) => state.user.role;
