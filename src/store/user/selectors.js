852b065f19959768b5dec3f814726c66c5f0dabf
andriuhakrava <andriuhakrava@gmail.com>
2021-12-15 12:37:22 +0200

    add user role selector

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,3 +1,10 @@
+import { ROLE } from './roles';
+
 export const getAuthenticatedToken = (state) => state.user.token;
+
 export const getAuthenticatedUserName = (state) => state.user.name;
-export const getUserRole = (state) => state.user.role;
+
+export const getAuthenticatedStatus = (state) => state.user.isAuth;
+
+export const getRoleAdmin = (state) =>
+    state.user.role === ROLE.ADMIN ? true : false;
