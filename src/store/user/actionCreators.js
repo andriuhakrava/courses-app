f10ca6e450a59016c200732200d78149d0197127
andriuhakrava <andriuhakrava@gmail.com>
2021-12-16 11:04:33 +0200

    add logout, authenticate action creators

--- src/store/user/actionCreators.js
+++ src/store/user/actionCreators.js
@@ -1,23 +1,26 @@
-import {
-	removeToken,
-	removeUserName,
-} from '../../helpers/localStorageHandler.js';
+import { getToken } from '../../helpers/localStorageHandler.js';
 
-import { LOGIN_USER, LOGOUT_USER, SET_USER_ROLE } from './actionTypes.js';
+import { LOGIN_USER, LOGOUT_USER, AUTHENTICATE_USER } from './actionTypes.js';
 
 export const logInUser = (response) => ({
 	type: LOGIN_USER,
 	payload: response,
 });
 
-export const logOutUser = () => {
-	removeToken();
-	removeUserName();
-
-	return { type: LOGOUT_USER };
-};
-
-export const setUserRole = (role) => ({
-	type: SET_USER_ROLE,
-	payload: role,
+export const logOutUser = () => ({
+	type: LOGOUT_USER,
 });
+
+export const authenticateUser = (user) => {
+	const token = getToken();
+
+	const result = {
+		...user,
+		token,
+	};
+
+	return {
+		type: AUTHENTICATE_USER,
+		payload: result,
+	};
+};
