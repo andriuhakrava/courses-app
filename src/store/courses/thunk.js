c1a83c393e3c78f29f8736b5b37a21baa8403258
andriuhakrava <andriuhakrava@gmail.com>
2021-12-17 11:27:24 +0200

    add delete course, add course thunks

--- src/store/courses/thunk.js
+++ src/store/courses/thunk.js
@@ -1,8 +1,35 @@
-import { fetchCourses } from '../../services';
-import { coursesLoaded } from './actionCreators';
+import {
+	fetchCourses,
+	addCourse,
+	deleteCourse,
+	editCourse,
+} from '../../services';
+import { coursesLoaded, courseAdded, courseDeleted } from './actionCreators';
 
 export const fetchCoursesThunk = () => async (dispatch) => {
 	const courses = await fetchCourses();
 
-	dispatch(coursesLoaded(courses[0]));
+	if (courses) {
+		dispatch(coursesLoaded(courses));
+	}
+};
+
+export const addCourseThunk = (course) => async (dispatch) => {
+	const result = await addCourse(course);
+
+	if (result) {
+		dispatch(courseAdded(result));
+	}
+};
+
+export const deleteCourseThunk = (id) => async (dispatch) => {
+	await deleteCourse(id);
+
+	dispatch(courseDeleted(id));
+};
+
+export const editCourseThunk = (data) => async (dispatch) => {
+	const response = await editCourse(data);
+
+	console.log('EDIT RESPONSE IN THUNK', response);
 };
