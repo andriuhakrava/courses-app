de2378144c8730ae74f0438bdb54a00ebbc2ad83
andriuhakrava <andriuhakrava@gmail.com>
2022-01-18 11:38:56 +0200

    reorder mocked state from store to project components files

--- src/components/CourseForm/tests/courseForm.test.js
+++ src/components/CourseForm/tests/courseForm.test.js
@@ -3,7 +3,49 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import CourseForm from '../CourseForm';
 import Button from '../../../common/Button/Button';
-import mockedStore from '../../../store/tests/mockedStore.js';
+
+const mockedState = {
+	user: {
+		isAuth: true,
+		name: 'Test Name',
+		email: 'Test Email',
+		role: '',
+		token: 'Test Token',
+	},
+	courses: [
+		{
+			id: '1',
+			title: 'Course 1',
+			description: 'Course 1 description',
+			duration: 120,
+			creationDate: '08/12/21',
+			authors: ['author-1', 'author-3'],
+		},
+		{
+			id: '2',
+			title: 'Course 2',
+			description: 'Course 2 description',
+			duration: 500,
+			creationDate: '10/12/21',
+			authors: ['author-1', 'author-3', 'author-2'],
+		},
+		{
+			id: '3',
+			title: 'Course 3',
+			description: 'Course 3 description',
+			duration: 280,
+			creationDate: '04/12/21',
+			authors: ['author-1'],
+		},
+	],
+	authors: ['author-1', 'author-2', 'author-3'],
+};
+
+const mockedStore = {
+	getState: () => mockedState,
+	subscribe: jest.fn(),
+	dispatch: jest.fn(),
+};
 
 test('displays authors list in CourseForm component', () => {
 	render(
