bedd4442846b0b9414fa841869cb6bf4a3e7ee1e
andriuhakrava <andriuhakrava@gmail.com>
2022-01-25 17:15:59 +0200

    add mocked data files

--- src/components/Courses/tests/data.js
+++ src/components/Courses/tests/data.js
@@ -1,19 +1,19 @@
 const mockedState = {
-    user: {
-        isAuth: true,
-        name: 'Test Name',
-        email: 'Test Email',
-        role: '',
-        token: 'Test Token',
-    },
-    courses: [],
-    authors: [],
+	user: {
+		isAuth: true,
+		name: 'Test Name',
+		email: 'Test Email',
+		role: '',
+		token: 'Test Token',
+	},
+	courses: [],
+	authors: [],
 };
 
 const mockedStore = {
-    getState: () => mockedState,
-    subscribe: jest.fn(),
-    dispatch: jest.fn(),
+	getState: () => mockedState,
+	subscribe: jest.fn(),
+	dispatch: jest.fn(),
 };
 
 export default mockedStore;
